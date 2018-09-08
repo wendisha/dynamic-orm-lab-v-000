f788fea2093be0f202220aea0171ece47cb8836a
Wendy Calderon <wendycalderon@gmail.com>
2018-09-08 02:44:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,4 +46,9 @@ def initialize(options={})
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+  
+    def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 end
