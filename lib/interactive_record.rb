1fef7bb9606e19f044956d4eb455ee5508b53c90
Wendy Calderon <wendycalderon@gmail.com>
2018-09-08 02:34:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -24,4 +24,20 @@ class InteractiveRecord
       self.send("#{property}=", value)
     end
   end
+  
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
 end
